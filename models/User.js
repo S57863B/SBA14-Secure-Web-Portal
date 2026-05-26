72ecbcdfe25c74ccb0cd5255af6d7019730b94c3
S57863B <i349cjhswe@gmail.com>
2026-05-25 21:14:51 -0400

    Added user model with bcrypt hashing

---
+++ models/User.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const SALT_ROUNDS = 12;
+
+const UserSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: false,
+      minlength: [6, 'Password must be at least 6 characters'],
+    },
+    githubId: {
+      type: String,
+      required: false,
+    },
+    displayName: {
+      type: String,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password') || !this.password) {
+    return next();
+  }
+  try {
+    this.password = await bcrypt.hash(this.password, SALT_ROUNDS);
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+UserSchema.methods.comparePassword = async function (candidatePassword) {
+  if (!this.password) return false;
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model('User', UserSchema);
